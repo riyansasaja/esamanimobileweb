0384d365a99c29432122b3fc812e3f82daf9824d
riyanboyuhu <riyanboyuhu@gmail.com>
2024-05-11 13:47:59 +0800

    menyimpan data di session dan menampilkan, serta menampilkan hari tanggal bulan dan jam

--- asssets/home.js
+++ asssets/home.js
@@ -1,3 +1,86 @@
 let session_login = sessionStorage.getItem('session_login');
 let dataUser = JSON.parse(session_login);
-console.log(dataUser.nama);
+
+$("#nama").text(dataUser.nama);
+$("#nip").text(`NIP. ${dataUser.nip}`);
+
+//menjalankan fungsi jam
+window.setTimeout("waktu()", 1000);
+//menampilkan tanggal
+tampilTanggal();
+
+
+//on click bsen siang
+$('#absensiang').on('click', function () {
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function untik menampilkan tanggal
+function tampilTanggal() {
+
+//mengambil lhari tanggal bulan untuk ditampilkan 
+let date = new Date()
+let tahun = date.getFullYear();
+let bulan = date.getMonth();
+let tanggal = date.getDate ();
+let hari = date.getDay();
+
+//mengganti hari ke indonesia
+switch (hari) {
+    case 0: hari = "Minggu"; break;
+    case 1: hari = "Senin"; break;
+    case 2: hari = "Selasa"; break;
+    case 3: hari = "Rabu"; break;
+    case 4: hari = "Kamis"; break;
+    case 5: hari = "Jumat"; break;
+    case 6: hari = "Sabtu"; break;
+}
+//mengganti bulan ke indonesia
+switch(bulan) {
+    case 0: bulan = "Januari"; break;
+    case 1: bulan = "Februari"; break;
+    case 2: bulan = "Maret"; break;
+    case 3: bulan = "April"; break;
+    case 4: bulan = "Mei"; break;
+    case 5: bulan = "Juni"; break;
+    case 6: bulan = "Juli"; break;
+    case 7: bulan = "Agustus"; break;
+    case 8: bulan = "September"; break;
+    case 9: bulan = "Oktober"; break;
+    case 10: bulan = "November"; break;
+    case 11: bulan = "Desember"; break;
+   }
+
+   //tampilkan hari tanggal bulan dan waktu
+   $("#hari").text(hari);
+   $("#tanggal").text(tanggal);
+   $("#bulan").text(bulan);
+   $("#tahun").text(tahun);
+    
+}
+   //function untuk menampilkan jam
+function waktu() {
+    var waktu = new Date();
+    setTimeout("waktu()", 1000);
+    document.getElementById("jam").innerHTML = waktu.getHours();
+    document.getElementById("menit").innerHTML = waktu.getMinutes();
+    document.getElementById("detik").innerHTML = waktu.getSeconds();
+}
+
