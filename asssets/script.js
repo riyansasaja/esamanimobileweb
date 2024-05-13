8c6fbd61d5a9b58d24c8b72966d11b65f1612388
riyanboyuhu <riyanboyuhu@gmail.com>
2024-05-13 13:57:58 +0800

    tambahan meta tag untuk http -12

--- asssets/script.js
+++ asssets/script.js
@@ -29,7 +29,7 @@ $('#btn_masuk').on('click', function () {
                     text: 'Nip Tidak Terdaftar, Silahkan hubungi admin!',
                     icon: "error"
                   });
-                  return;
+              
             }
         },
         success: function (data, status, xhr) {
